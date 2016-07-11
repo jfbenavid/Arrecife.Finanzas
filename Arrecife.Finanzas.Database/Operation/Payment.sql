fc820d89841d221313604c8f735ec04ae9c56b88
jfbenavid <jfbenavid@hotmail.com>
2016-07-11 17:34:03 -0500

    Start
    Se crea el inicio de la solucion empezando con el proyecto de base de
    datos

---
+++ Arrecife.Finanzas.Database/Operation/Payment.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [Operation].[Payment]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [CitizenshipCard] INT NOT NULL, 
+    [Receipt] INT NOT NULL, 
+    [ValureReceipt] DECIMAL(10) NOT NULL, 
+    [Notes] VARCHAR(4000) NOT NULL, 
+    [InputSystemDate] DATETIME NOT NULL, 
+    [ModifiedDate] DATETIME NOT NULL, 
+    [User] INT NOT NULL
+)
